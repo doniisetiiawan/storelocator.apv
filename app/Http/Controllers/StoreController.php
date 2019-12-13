c8c79b569fe0789c181255aa7d0090432f605abb
Doni Setiawan <donisetiawan.050@gmail.com>
2019-12-13 18:35:36 +0700

    Creating an API method for deleting a store client

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -119,14 +119,16 @@ class StoreController extends Controller
         }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $store = Store::find($id);
+
+        if (!empty($store))
+            $store->delete();
+        return \Response::json([
+            'error' => false,
+            'msg'   => 'Store deleted'],
+            200
+        )->setCallback(Input::get('callback'));
     }
 }
