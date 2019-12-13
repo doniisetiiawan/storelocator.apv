9debe5d7dfc95010b837288858d2fbe6295998b5
Doni Setiawan <donisetiawan.050@gmail.com>
2019-12-13 18:18:00 +0700

    Creating an API method for searching the stores

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -10,9 +10,21 @@ class StoreController extends Controller
 {
     public function index()
     {
-        $stores = Store::all();
+        $city    = Input::get('city');
+        $country = Input::get('country');
+        $zip     = Input::get('zip');
+
+        If ($city == '' && $country == '' && $zip == '') {
+            $stores = Store::all();
+        } else {
+            $stores = Store::where('city', 'LIKE', "%$city%")
+                ->orWhere('country', 'LIKE', "%$country%")
+                ->orWhere('zip', '=', "$zip")
+                ->get();
+        }
+
         return \Response::json([
-            'error' => false,
+            'error'  => false,
             'stores' => $stores->toArray()],
             200
         )->setCallback(Input::get('callback'));;
@@ -31,7 +43,7 @@ class StoreController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -52,7 +64,7 @@ class StoreController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -63,8 +75,8 @@ class StoreController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -75,7 +87,7 @@ class StoreController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
